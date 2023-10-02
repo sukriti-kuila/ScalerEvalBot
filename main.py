144ade3798b9363590d5bfd8de615c601882bcb7
Sukriti Kuila <sukritikuila22@gmail.com>
2023-10-02 12:43:39 +0530

    Fetch Message from channels

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import discord
+import pandas as pd
 from discord.ext import commands
 
 from apikeys import *
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+guild = discord.Guild
 
 @client.event
 async def on_ready():
     print("The bot is now ready for use")
     print("-----------------------------")
 
-@client.command()
-async def hello(ctx):
-    await ctx.send("Hello, I am Bot")
+@client.event
+async def on_message(message):
+    print("Author of the msg",message.author)
+    print("-----------------------------")
+    if message.author == client.user:
+        return 
+    elif message.content.startswith('!bot'):
+        print("Author: ",message.author," Content: ",message.content+" Channel: ",message.channel," Created: ",message.created_at)
+        await message.channel.send("Hi from ScalerEvalBot")
 
 client.run(DISCORD_BOT_TOKEN)
 
