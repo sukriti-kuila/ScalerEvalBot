5a7310943a1d0612bc9ee36fcdbebb94dc3b148b
Sukriti Kuila <sukritikuila22@gmail.com>
2023-10-05 10:40:52 +0530

    Read Participants data for an event

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 from discord.ext import commands
+from decouple import config
 
-from apikeys import *
 from utils import *
+
+#token
+discord_api_key = config('DISCORD_BOT_TOKEN')
 
 # initialize bot
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
@@ -17,11 +20,16 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return 
-    message_str = str(message.content).split("\n")
-    print(message_str)
-    response = await fomatting_check(message_str)
-    await message.channel.send(response)
+        return
+    if message.content.lower().startswith("!evalbot new event"):
+        if message.author.id == message.channel.guild.owner_id:
+            response = await eventData(message)
+            await message.channel.send(response)
+    elif message.content.lower().startswith("!evalbot"):
+        message_str = str(message.content).split("\n")
+        print(message_str)
+        response = await fomatting_check(message_str)
+        await message.channel.send(response)
 
 @client.event
 async def on_message_edit(before, after):
@@ -32,6 +40,6 @@ async def on_message_edit(before, after):
     await after.channel.send (response)
 
 # it tells the bot to run
-client.run(DISCORD_BOT_TOKEN)
+client.run(discord_api_key)
 
 
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import re
 from datetime import datetime, timedelta
 import pytz
+import csv
 
 async def fomatting_check(message_str):
     if(len(message_str) < 2):
@@ -9,6 +10,7 @@ async def fomatting_check(message_str):
         first_line = message_str[0].split()
         bot_command = first_line[0].lower()
         completion_command = first_line[1 : len(first_line)]
+        print(first_line," ",len(first_line))
 
         Second_line = message_str[1]
         post_pattern = r'^social media link : https://(www.linkedin.com|twitter.com)/+'
@@ -24,13 +26,34 @@ async def fomatting_check(message_str):
 
         user_day_no = int(completion_command[1][3:len(completion_command[1])])
 
-        if len(first_line) == 3 and bot_command == "!bot" and completion_command[0].lower() == "completed" and completion_command[1][0:3].lower() == "day" and (0 < int(completion_command[1][3:len(completion_command[1])]) <= challenge_days) and  user_day_no== day_no:
+        if len(first_line) == 3 and bot_command == "!evalbot" and completion_command[0].lower() == "completed" and completion_command[1][0:3].lower() == "day" and (0 < int(completion_command[1][3:len(completion_command[1])]) <= challenge_days) and  user_day_no== day_no:
             if re.match(post_pattern, Second_line.lower()):
                 return "YOU HAVE SUCCESSFULLY COMPLETED TODAY'S TASK :partying_face: "
             else:
-                return "------------------\nYou have made a formatting mistake in line 2\nEdit the previous message or send a new message using the following format for line2\nsocial media link : <linkedin/twitter post> (Case-Insensitive)\n------------------"
+                return "------------------\nYou have made a formatting mistake in line 2\nEdit the previous message or send a new message using the following format for line2\n\nsocial media link : <linkedin/twitter post> (Case-Insensitive)\n------------------"
 
         else:
             if user_day_no != day_no:
                 return "------------------\nYou have made a formatting mistake in line 1\nLooks like the day number is wrong\nEdit the previous message or send a new message to rectify the mistake\n------------------"
-            return "------------------\nYou have made a formatting mistake in line 1\nEdit the previous message or send a new message using the following format for line1\n!bot completed day<day no> (Case-Insensitive)\n------------------"
+            return "------------------\nYou have made a formatting mistake in line 1\nEdit the previous message or send a new message using the following format for line1\n\n!Evalbot completed day<day no> (Case-Insensitive)\n------------------"
+
+async def eventData(message):
+        message_str = str(message.content).split("\n")
+        if len(message_str)==4:
+            event_name = message_str[1]
+            start_date = message_str[2]
+            event_duration = message_str[3]
+            print (f"name {event_name} start {start_date} duration {event_duration}")
+            if message.attachments:
+                attachment = message.attachments[0]
+                if attachment.filename.endswith('.csv'):
+                    # Download the CSV file
+                    filename = event_name+""+".csv"
+                    await attachment.save(filename)
+                    return "Entries of participants have been done"
+                else:
+                    return "Please attach a CSV file"
+            else:
+                return "Please attach a CSV file"
+        else:
+            return "Please follow the proper format to register data"
