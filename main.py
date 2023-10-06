cd2ad9333a789eed337f8e9a3cab74221e5be8f1
Ayan Bhunia <bhuniaayan2002@gmail.com>
2023-10-06 15:27:15 +0530

    Merge conflict - main.py
    Signed-off-by: Ayan Bhunia <bhuniaayan2002@gmail.com>

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import discord
 from discord.ext import commands
-<<<<<<< HEAD
-
-from apikeys import *
-from utils import *
-=======
 from decouple import config
 
 from utils import *
 
 #token
 discord_api_key = config('DISCORD_BOT_TOKEN')
->>>>>>> 1a37b971919c4559b89ca1cac48dcfc231760620
 
 # initialize bot
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
@@ -26,13 +20,6 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-<<<<<<< HEAD
-        return 
-    message_str = str(message.content).split("\n")
-    print(message_str)
-    response = await fomatting_check(message_str)
-    await message.channel.send(response)
-=======
         return
     # Reading participant data
     if message.content.lower().startswith("!evalbot new event"):
@@ -43,17 +30,11 @@ async def on_message(message):
     elif message.content.lower().startswith("!evalbot"):
         response = await fomatting_check(message)
         await message.channel.send(response)
->>>>>>> 1a37b971919c4559b89ca1cac48dcfc231760620
 
 @client.event
 async def on_message_edit(before, after):
     if after.author == client.user:
         return
-<<<<<<< HEAD
-    message_str = str(after.content).split("\n")
-    response = await fomatting_check(message_str)
-    await after.channel.send (response)
-=======
     if after.content.lower().startswith("!evalbot new event"):
         if after.author.id == after.channel.guild.owner_id:
             response = await eventData(after)
@@ -61,9 +42,6 @@ async def on_message_edit(before, after):
     elif after.content.lower().startswith("!evalbot"):
         response = await fomatting_check(after)
         await after.channel.send (response)
->>>>>>> 1a37b971919c4559b89ca1cac48dcfc231760620
 
 # it tells the bot to run
 client.run(discord_api_key)
-
-
