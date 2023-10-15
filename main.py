e086bd8ed1104ad499de04c889248802d9c200d7
Sukriti Kuila <sukritikuila22@gmail.com>
2023-10-15 22:39:57 +0530

    Add Slash command and Update Token

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-import discord
-from discord.ext import commands
-from decouple import config
-import pandas as pd
-
 from utils import *
+from libraries import *
 
 #token
 discord_api_key = config('DISCORD_BOT_TOKEN')
 
 # initialize bot
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+client = commands.Bot(command_prefix="!", intents=nextcord.Intents.all())
+
+@tasks.loop()
+async def main():
+    current_time = datetime.now(pytz.timezone('Asia/Kolkata'))
+    target_time = current_time.replace(hour=22, minute=28, second=0)
+    print("soemthinggg")
+    if current_time > target_time:
+        target_time += timedelta(days=1)
+
+    initial_delay = (target_time - current_time).total_seconds()
+    print("Initial delay ",initial_delay)
+    await asyncio.sleep(initial_delay)
+
+    print ("After await")
+    while True:
+        response = await set_reminder()
+        for ele in response:
+            for key,value in ele.items():
+                user = client.get_user(key)
+                if user:
+                    event_str = ""
+                    for events in value:
+                        event_str += events+"  "
+                    await user.send(f"**REMINDER**\nYou haven't yet posted today's task post for the event(s) {event_str}")
+                else:    
+                    print("User not found")
+        await asyncio.sleep(3600)
 
 # here, on_ready() tells that bot is ready to receive command
 # This is for testing
@@ -17,6 +40,7 @@ client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 async def on_ready():
     print("The bot is now ready for use")
     print("-----------------------------")
+    main.start()
 
 @client.event
 async def on_message(message):
@@ -28,12 +52,12 @@ async def on_message(message):
         if message.author.id == message.channel.guild.owner_id:
             response = await eventData(message)
             if response["success"]:
-                embed = discord.Embed(title="EVENT ADDED!", description=response["message"], color=0x006600, timestamp = message.created_at)
+                embed = nextcord.Embed(title="EVENT ADDED!", description=response["message"], color=0x006600, timestamp = message.created_at)
             else:
-                embed = discord.Embed(title="Failed to add event", description=response["message"], color=0xcc0000, timestamp = message.created_at) 
+                embed = nextcord.Embed(title="Failed to add event", description=response["message"], color=0xcc0000, timestamp = message.created_at) 
             await message.channel.send(embed=embed)
         else:
-            embed = discord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
+            embed = nextcord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
             await message.channel.send(embed=embed)
 
     # Delete event from database
@@ -42,12 +66,12 @@ async def on_message(message):
             response = await delete_event(message)
             print(response)
             if response["success"]:
-                embed = discord.Embed(title="EVENT DELETED", description=response["message"], color=0x34eb71, timestamp = message.created_at)   
+                embed = nextcord.Embed(title="EVENT DELETED", description=response["message"], color=0x34eb71, timestamp = message.created_at)   
             else:
-                embed = discord.Embed(title="Something went wrong", description=response["message"], color=0xe60000, timestamp = message.created_at)
+                embed = nextcord.Embed(title="Something went wrong", description=response["message"], color=0xe60000, timestamp = message.created_at)
             await message.channel.send(embed=embed)
         else:
-            embed = discord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
+            embed = nextcord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
             await message.channel.send(embed=embed)
 
     # Export Eligible Participants' list in csv format
@@ -57,23 +81,54 @@ async def on_message(message):
             if response["success"]:
                 owner = message.author
                 await owner.send(file=response["message"])
-                embed = discord.Embed(title="CSV FILE GENERATED", description="File has been sent to your DM", color=0x34eb71, timestamp = message.created_at)
+                embed = nextcord.Embed(title="CSV FILE GENERATED", description="File has been sent to your DM", color=0x34eb71, timestamp = message.created_at)
                 await message.channel.send(embed=embed)
             else:
-                embed = discord.Embed(title="Something went wrong", description=response["message"], color=0xe60000, timestamp = message.created_at)
+                embed = nextcord.Embed(title="Something went wrong", description=response["message"], color=0xe60000, timestamp = message.created_at)
                 await message.channel.send(embed=embed)
         else:
-            embed = discord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
+            embed = nextcord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
             await message.channel.send(embed=embed)
     
+    # Update tokens 
+    elif message.content.lower().startswith("!evalbot update token"):
+        if message.author.id == message.channel.guild.owner_id:
+            response = await updateToken(message)
+            if response["success"]:
+                if len(response["updated"]):
+                    await message.channel.send(f"**Following users tokens have been updated**")
+                    for userid in response["updated"]:
+                        await message.channel.send(f"<@{userid}>")
+                        if len(response["failed"]):
+                            await message.channel.send(f"\n\n**Following users tokens can't be updated**")
+                            for userid in response["failed"]:
+                                await message.channel.send(f"<@{userid}>")
+
+            else:
+                embed = nextcord.Embed(title="TOKENS UPDATION FAILED", description=response["message"], color=0xe60000, timestamp = message.created_at)
+                await message.channel.send(embed=embed)
+
+        else:
+            embed = nextcord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
+            await message.channel.send(embed=embed)
+    
+    # 0-------------------------------------------------------
+    # elif message.content.lower().startswith("!evalbot abcd"):
+    #     if message.author.id == message.channel.guild.owner_id:
+    #         response = await set_reminder()
+    #         for ele in response:
+    #             for key,value in ele.items():
+    #                 print (key," ",value)
+
+
     # Checking the desired Format
     elif message.content.lower().startswith("!evalbot"):
         response = await fomatting_check(message)
         if response["success"]:
             response = f'**{message.author.display_name.upper()}**, {response["message"]}'
-            embed = discord.Embed(title="CORRECT FORMAT", description=response, color=0x4dff4d, timestamp = message.created_at)
+            embed = nextcord.Embed(title="CORRECT FORMAT", description=response, color=0x4dff4d, timestamp = message.created_at)
         else:
-            embed = discord.Embed(title="INCORRECT FORMAT", description=response["message"], color=0xe60000, timestamp = message.created_at)     
+            embed = nextcord.Embed(title="INCORRECT FORMAT", description=response["message"], color=0xe60000, timestamp = message.created_at)     
         await message.channel.send(embed=embed)
 
 
@@ -87,12 +142,12 @@ async def on_message_edit(before, after):
         if after.author.id == after.channel.guild.owner_id:
             response = await eventData(after)
             if response["success"]:
-                embed = discord.Embed(title="EVENT ADDED!", description=response["message"], color=0x006600, timestamp = after.created_at)
+                embed = nextcord.Embed(title="EVENT ADDED!", description=response["message"], color=0x006600, timestamp = after.created_at)
             else:
-                embed = discord.Embed(title="Failed to add event", description=response["message"], color=0xcc0000, timestamp = after.created_at) 
+                embed = nextcord.Embed(title="Failed to add event", description=response["message"], color=0xcc0000, timestamp = after.created_at) 
             await after.channel.send(embed=embed)
         else:
-            embed = discord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = after.created_at) 
+            embed = nextcord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = after.created_at) 
             await after.channel.send(embed=embed)
 
     # Delete event from database
@@ -100,12 +155,12 @@ async def on_message_edit(before, after):
         if after.author.id == after.channel.guild.owner_id:
             response = await delete_event(after)
             if response["success"]:
-                embed = discord.Embed(title="EVENT DELETED", description=response["message"], color=0x34eb71, timestamp = after.created_at)   
+                embed = nextcord.Embed(title="EVENT DELETED", description=response["message"], color=0x34eb71, timestamp = after.created_at)   
             else:
-                embed = discord.Embed(title="Something went wrong", description=response["message"], color=0xe60000, timestamp = after.created_at)
+                embed = nextcord.Embed(title="Something went wrong", description=response["message"], color=0xe60000, timestamp = after.created_at)
             await after.channel.send(embed=embed)
         else:
-            embed = discord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
+            embed = nextcord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
             await after.channel.send(embed=embed)
 
     # Export Eligible Participants' list in csv format
@@ -115,24 +170,61 @@ async def on_message_edit(before, after):
             if response["success"]:
                 owner = after.author
                 await owner.send(file=response["message"])
-                embed = discord.Embed(title="CSV FILE GENERATED", description="File has been sent to your DM", color=0x34eb71, timestamp = after.created_at)
+                embed = nextcord.Embed(title="CSV FILE GENERATED", description="File has been sent to your DM", color=0x34eb71, timestamp = after.created_at)
                 await after.channel.send(embed=embed)
             else:
-                embed = discord.Embed(title="Something went wrong", description=response["message"], color=0xe60000, timestamp = after.created_at)
+                embed = nextcord.Embed(title="Something went wrong", description=response["message"], color=0xe60000, timestamp = after.created_at)
                 await after.channel.send(embed=embed)
         else:
-            embed = discord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = after.created_at) 
+            embed = nextcord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = after.created_at) 
             await after.channel.send(embed=embed)
 
+    # Update tokens
+    elif after.content.lower().startswith("!evalbot update token"):
+        if after.author.id == after.channel.guild.owner_id:
+            response = await updateToken(after)
+            if response["success"]:
+                if len(response["updated"]):
+                    await after.channel.send(f"**Following users tokens have been updated**")
+                    for userid in response["updated"]:
+                        await after.channel.send(f"<@{userid}>")
+                        if len(response["failed"]):
+                            await after.channel.send(f"\n\n**Following users tokens can't be updated**")
+                            for userid in response["failed"]:
+                                await after.channel.send(f"<@{userid}>")
+            else:
+                embed = nextcord.Embed(title="TOKENS UPDATION FAILED", description=response["message"], color=0xe60000, timestamp = message.created_at)
+                await after.channel.send(embed=embed)
+
+        else:
+            embed = nextcord.Embed(title="PERMISSION DENIED", description="Sorry, you're not authorized to perform this command", color=0xcc0000, timestamp = message.created_at) 
+            await after.channel.send(embed=embed)
+                
     #fomatting check
     elif after.content.lower().startswith("!evalbot"):
         response = await fomatting_check(after)
         if response["success"]:
             response = f'**{after.author.name.upper()}**, {response["message"]}'
-            embed = discord.Embed(title="CORRECT FORMAT", description=response, color=0x4dff4d, timestamp = after.created_at)
+            embed = nextcord.Embed(title="CORRECT FORMAT", description=response, color=0x4dff4d, timestamp = after.created_at)
         else:
-            embed = discord.Embed(title="INCORRECT FORMAT", description=response["message"], color=0xe60000, timestamp = after.created_at)     
+            embed = nextcord.Embed(title="INCORRECT FORMAT", description=response["message"], color=0xe60000, timestamp = after.created_at)     
         await after.channel.send(embed=embed)
+
+# Slash commands
+testserverid = 1081631255700963409
+@client.slash_command(name="help",description="List of all commands",guild_ids=[testserverid])
+async def help (interaction: Interaction):
+    embed = nextcord.Embed(title="**All Commands**",color=0x03f4fc)
+
+    embed.add_field(name="🤖 DAILY POST FORMAT CHECK", value=await format_check_command(), inline=False)
+    embed.add_field(name="🤖 ADD EVENT [OWNER ONLY]", value=await add_event_command(), inline=False)
+    embed.add_field(name="🤖 DELETE EVENT [OWNER ONLY]", value=await delete_event_command(), inline=False)
+    embed.add_field(name="🤖 RESULT OF EVENT [OWNER ONLY]", value=await result_event_command(), inline=False)
+    embed.add_field(name="🤖 TOKEN UPDATE [OWNER ONLY]", value=await update_token_event_command(), inline=False)
+
+    await interaction.response.send_message(embed=embed)
+
+
 
 # it tells the bot to run
 client.run(discord_api_key)
